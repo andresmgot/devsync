28002004345f4055c1502b1051142fdfb58480f7
Andres <andres@bitnami.com>
2016-05-28 18:41:03 +0200

    Added support for .ignore file. Sync project folder

--- index.js
+++ index.js
@@ -5,14 +5,19 @@ const path = require('path');
 const config = JSON.parse(fs.readFileSync(path.join(__dirname, 'config.json')));
 const exec = require('child_process').exec;
 const toTransferFile = path.join(__dirname, 'to-transfer.json');
+const ignoreFile = path.join(__dirname, '.ignore');
 const _ = require('lodash');
 const filesBeingTransfered = [];
+let syncInProgress = false;
 let filesToTransfer = [];
+let filesToIgnore = [];
 try {
-  const _files = JSON.parse(fs.readFileSync(toTransferFile));
-  filesToTransfer = _files;
-} catch(e) { /* not empty */ }
+  filesToTransfer = JSON.parse(fs.readFileSync(toTransferFile, 'utf8'));
+} catch (e) {/* not empty */}
 
+try {
+  filesToIgnore = _.compact(fs.readFileSync(ignoreFile, 'utf8').split('\n'));
+} catch(e) {/* not empty */}
 
 function addToList(file) {
   if(!_.includes(filesToTransfer, file)) {
@@ -21,22 +26,23 @@ function addToList(file) {
   }
 }
 
-function removeFromList(file) {
+function removeFromList(file, options) {
+  options = _.defaults(options || {}, {all: false});
   if(_.includes(filesToTransfer, file)) { 
     _.pull(filesToTransfer, file);
     fs.writeFileSync(toTransferFile, filesToTransfer);
   }
 }
 
-function sync(file, remotePath, host, port, user){
+function scp(file, remotePath, host, port, user){
   if (!_.includes(filesBeingTransfered, file)) {
     filesBeingTransfered.push(file);
-    exec(`scp -o Port=${port} ${path.join(config.localDir, file)} ${user}@${host}:${remotePath}`,
+    exec(`scp -r -o Port=${port} ${path.join(config.localDir, file)} ${user}@${host}:${remotePath}`,
          (error, stdout, stderr) => {
            _.pull(filesBeingTransfered, file); 
            if (error) {
-             if (error.message.match('No such file or directory')) {
-               console.log(`File ${file} no longer exists`);
+             if (error.message.match('No such file or directory') || error.message.match('Permission denied')) {
+               console.log(`File ${file} no longer exists or it is not modificable`);
                removeFromList(file);
              } else {
                console.log('Failed to transfer, saving for later:', error.message);
@@ -51,24 +57,66 @@ function sync(file, remotePath, host, port, user){
   }
 }
 
+function sync(localDir, remoteDir, host, port, user){
+  if (!syncInProgress) {
+    syncInProgress = true;
+    exec(`rsync -rv -e "ssh -o Port=${port}" ${path.join(config.localDir, '*')} ${user}@${host}:${remoteDir}`,
+         (error, stdout, stderr) => {
+           syncInProgress = false;
+           if (error) {
+             console.log(`Failed to sync`);
+           }
+           console.log(stdout);
+           console.log(stderr);
+         });
+  }
+}
+
+function remoteRemove(file, host, port, user) {
+  exec(`ssh -o Port=${port} ${user}@${host} 'rm -r ${file}'`,
+       (error) => {
+         console.log(error);
+       });
+}
+
 fs.watch(config.localDir, {recursive: true, encoding: 'buffer'}, (event,filename) => {
-  if (filename && !filename.match('#') && !filename.match(/.*~/) && !filename.match(/.*lock/)) {
-    sync(filename,
-         path.join(config.remoteDir, filename),
-         config.host, config.port, config.user);
+  if (event === 'rename') {
+    fs.access(path.join(config.localDir, filename), fs.F_OK, (err) => {
+      if(err) {
+        // File removed, deleting remotely
+        remoteRemove(path.join(config.remoteDir, filename),
+                     config.host, config.port, config.user);
+      } else {
+        if (_.every(filesToIgnore, (f) => !filename.match(f))) {
+          if (syncInProgress) {
+            addToList(filename);
+          } else {
+            scp(filename,
+                path.join(config.remoteDir, filename),
+                config.host, config.port, config.user);
+          }
+        }
+      }
+    });
   }
 });
 
 setInterval(function() {
   console.log('Checking files to transfer:');
   console.log(filesToTransfer);
-  if (!_.isEmpty(filesToTransfer)) {
+  if (!_.isEmpty(filesToTransfer) && !syncInProgress) {
     _.each(filesToTransfer, file => {
-      sync(file,
+      scp(file,
            path.join(config.remoteDir, file),
            config.host, config.port, config.user);
     });
   }
 }, 30000);
+
+setInterval(function() {
+  console.log('Re-synchying directories');
+  sync(config.localDir, config.remoteDir,
+       config.host, config.port, config.user);
+}, 600000);
 
 
